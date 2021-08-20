74c0a216dda344a9b72e6f52ebe78a11ec8df304
Dara Akitoye <daraakitoye1@gmail.com>
2021-08-20 19:10:23 +0000

    adds checks to see if word is found both vertically and horizontally

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+//pair programming with @akshathakulkarni
+
+const transpose = require('./transpose');
+
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) {
+        return undefined;
     }
+    const transposed = transpose(letters);
+
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+
+    const verticalJoin = transposed.map(ls => ls.join(''));
+    for (l of verticalJoin) {
+
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
